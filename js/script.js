2c8ce0e134072397d953ddd9a8dddee1802ad58d
dunbark3 <dunbark3@wwu.edu>
2025-04-12 10:04:17 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 function revealOne() {
     const images = document.getElementById('gojo1');
-    images.src = 'images/gj1.HEIC';
+    images.src = 'images/gj1.jpg';
 }
 
 function revealTwo() {
     const images = document.getElementById('rengoku');
-    images.src = 'images/kr.HEIC';
+    images.src = 'images/kr.jpg';
 }
 
 function revealThree() {
     const images = document.getElementById('ivan');
-    images.src = 'images/iv.HEIC';
+    images.src = 'images/iv.jpg';
 }
 
 function revealFour() {
     const images = document.getElementById('kazuha');
-    images.src = 'images/kz.HEIC';
+    images.src = 'images/kz.jpg';
 }
 
 function revealFive() {
     const images = document.getElementById('gojo2');
-    images.src = 'images/gj2.HEIC';
+    images.src = 'images/gj2.jpg';
 }
 
 function revealSix() {
     const images = document.getElementById('viktor');
-    images.src = 'images/vk.HEIC';
+    images.src = 'images/vk.jpg';
 }
 
 function revealSeven() {
     const images = document.getElementById('xiao');
-    images.src = 'images/xi.HEIC';
+    images.src = 'images/xi.jpg';
 }
