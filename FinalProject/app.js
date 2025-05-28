d5b973278ad30f5c6a1cd941cdddbad540d0f0cb
dunbark3 <dunbark3@wwu.edu>
2025-05-28 00:33:22 -0700

    Add files via upload

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -16,12 +16,68 @@ const db = getFirestore(app);
 const messagesRef = collection(db, 'messages');
 const chat = document.getElementById('messages');
 
+// Reveal the forms
+document.getElementById('signup').addEventListener('click', () => {
+    document.getElementById('signup_form').style.display = 'block';
+    document.getElementById('signup').style.display = 'none';
+    document.getElementById('signin').style.display = 'none';
+});
+document.getElementById('signin').addEventListener('click', () => {
+    document.getElementById('signin_form').style.display = 'block';
+    document.getElementById('signup').style.display = 'none';
+    document.getElementById('signin').style.display = 'none';
+});
+
+// Check for validation
+let user_name;
+
+const users = collection(db, 'users');
+document.getElementById('signup').addEventListener('submit', async function(event) {
+    const name = document.getElementById('name').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    const q = query(users, where("name", "==", name));
+    const search = await getDocs(q);
+
+    if(!search.empty) {
+        const user = search.docs[0].data();
+        const temp = doc(db, "users", user.id);
+
+        await updateDoc(temp, {
+            password: password
+        });
+        user_name = name;
+    } else {
+        window.location.href = 'chat.html';
+        console.log("No user found with that name.");
+    }
+});
+
+document.getElementById('signin').addEventListener('submit', async function(event) {
+    const name = document.getElementById('name').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    const q1 = query(users, where("name", "==", name));
+    const search = await getDocs(q);
+
+    if(!search.empty) {
+        const user = search.docs[0].data();
+
+        if(temp.password == password) {
+            user_name = name;
+        } else {
+            console.log("Invalid password.");
+        }
+        window.location.href = 'chat.html';
+    } else {
+        console.log("No user found with that name.");
+    }
+});
+
 document.getElementById('sendBtn').addEventListener('click', async () => {
-    const name = document.getElementById('nameInput').value.trim();
     const text = document.getElementById('messageInput').value.trim();
 
     await addDoc(messagesRef, {
-        name,
         text,
         timestamp: serverTimestamp()
     });
@@ -36,7 +92,7 @@ onSnapshot(q, (snapshot) => {
     snapshot.forEach(doc => {
         const message = doc.data();
         const text = document.createElement('div');
-        text.textContent = `${message.name}: ${message.text}`;
+        text.textContent = `${user_name}: ${message.text}`;
         chat.appendChild(text);
     });
 
