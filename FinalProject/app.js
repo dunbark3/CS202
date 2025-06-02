56bcb84aafae903d7953d39658cfde1ba95035ca
dunbark3 <dunbark3@wwu.edu>
2025-06-01 23:15:28 -0700

    Add files via upload

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import { getFirestore, collection, addDoc, query, orderBy, onSnapshot, serverTimestamp, where, getDocs, updateDoc, doc} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, query, orderBy, onSnapshot, serverTimestamp, where, getDoc, updateDoc, doc, setDoc} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDtU5uT1M82xdx6rKJC-X7tLmU8IJV9Oio",
@@ -41,4 +41,55 @@ onSnapshot(update, (snapshot) => {
     });
 
     chat.scrollTop = chat.scrollHeight;
+});
+
+const trackList = [
+    "/music/music1.mp3",
+    "/music/music2.mp3",
+    "/music/music3.mp3",
+    "/music/music4.mp3"
+];
+
+const playing = doc(db, "playback", "now_playing");
+
+let trackIndex = 0;
+
+async function startTrack(index) {
+    await setDoc(playing, {
+        currentTrack: index,
+        startedAt: serverTimestamp()
+    });
+}
+
+const player = document.getElementById("player");
+
+async function play() {
+    const snap = await getDoc(playing);
+
+    if (!snap.exists()) {
+        await startTrack(0);
+        return;
+    }
+  
+    const data = snap.data();
+    trackIndex = data.currentTrack;
+    const startedAt = data.startedAt.toMillis();
+    const now = Date.now();
+    const elapsed = (now - startedAt) / 1000;
+  
+    player.src = trackList[trackIndex];
+    
+    player.addEventListener("loadedmetadata", () => {
+        player.currentTime = elapsed;
+        player.play();
+    }, { once: true });
+}
+
+player.addEventListener("ended", async () => {
+    trackIndex = (trackIndex + 1) % trackList.length;
+    await startTrack(trackIndex);
+});
+
+document.getElementById("startPlaybackBtn").addEventListener("click", () => {
+    play();
 });
